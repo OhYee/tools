82a504a8bde801927ea328d2e7a1fe42c2266f40
OhYee <oyohyee@oyohyee.com>
2021-12-06 00:06:05 +0800

    import dir in addon

--- addon/__init__.py
+++ addon/__init__.py
@@ -3,7 +3,11 @@ from .base import addons
 
 addon_dir = os.path.split(os.path.realpath(__file__))[0]
 
-for _, _, files in os.walk(addon_dir):
+for dir, _, files in os.walk(addon_dir):
     for file in files:
         if file.endswith(".py") and file != "base.py" and file != "__init__.py":
-            exec("from .%s import *" % file[:-3])
+            if dir != addon_dir:
+                file = dir[len(addon_dir)+1:] + "/" + file
+            file = file[:-3].replace("/", ".")
+            print(file)
+            exec("from .%s import *" % file)
